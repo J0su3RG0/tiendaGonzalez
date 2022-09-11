338bddd21bde61877b563441187a1424a1a69876
Josue Gonzalez <DanteOSINT@protonmail.com>
2022-09-11 12:09:04 -0500

    Se añaden más animaciones a la seccion de Libros y Hardware, transiciones a los botones de redes sociales

--- js/bro.js
+++
@@ -1,9 +0,0 @@
-onEvent ("button1", "click", function(){
-    console.log("button1 clicket!");
-});
-onEvent ("image1", "click", function() {
-    console.log("ima1 clicked!");
-});
-onEvent("button2", "click", function(){
-    console.log("button2 clicked!");
-});
